92f1b6c9201dcafdc793ae15fa0d0bfb0dc4a799
Anton Zhaparov <zhaparoff@gmail.com>
2019-09-12 18:52:07 +0300

    Declaration updates

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,28 +1,26 @@
-declare namespace Alert {
-    interface Button {
-        label: string;
-        callback: () => void;
-        setFocus: boolean;
-        preventClose: boolean;
-    }
-
-    interface AlertStatic {
-        show(title?: string, message?: string, icon?: IconType, buttons?: ButtonList, width?: number, height?: number, padding?: number, preventCancel?: boolean): void;
-        showIFrame(iframeUrl: string, width?: number, height?: number, padding?: number, title?: string, buttons?: ButtonList, preventCancel?: boolean): void;
-        showWebResource(webResourceName: string, width?: number, height?: number, padding?: number, title?: string, buttons?: ButtonList, preventCancel?: boolean, baseUrl?: string): void;
-        showDialogProcess(dialogId: string, entityName: string, recordId: string, callback?: () => void, width?: number, height?: number, baseUrl?: string): void;
-        showLoading(): void;
-        hide(): void;
-        htmlEncode(text: string): string;
-        getCrmWindow(): Window | undefined;
-        getIFrameWindow(): Window | undefined;
-    }
-
-    type ButtonList = Button[];
-
-    type IconType = "INFO" | "WARNING" | "ERROR" | "SUCCESS" | "QUESTION" | "LOADING" | "NONE";
+interface Button {
+    label: string;
+    callback: () => void;
+    setFocus: boolean;
+    preventClose: boolean;
 }
 
-declare const Alert: Alert.AlertStatic;
+interface AlertStatic {
+    show(title?: string, message?: string, icon?: IconType, buttons?: ButtonList, width?: number, height?: number, padding?: number, preventCancel?: boolean): void;
+    showIFrame(iframeUrl: string, width?: number, height?: number, padding?: number, title?: string, buttons?: ButtonList, preventCancel?: boolean): void;
+    showWebResource(webResourceName: string, width?: number, height?: number, padding?: number, title?: string, buttons?: ButtonList, preventCancel?: boolean, baseUrl?: string): void;
+    showDialogProcess(dialogId: string, entityName: string, recordId: string, callback?: () => void, width?: number, height?: number, baseUrl?: string): void;
+    showLoading(): void;
+    hide(): void;
+    htmlEncode(text: string): string;
+    getCrmWindow(): Window | undefined;
+    getIFrameWindow(): Window | undefined;
+}
+
+type ButtonList = Button[];
+
+type IconType = "INFO" | "WARNING" | "ERROR" | "SUCCESS" | "QUESTION" | "LOADING" | "NONE";
+
+declare const Alert: AlertStatic;
 
 export default Alert;
